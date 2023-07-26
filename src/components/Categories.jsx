cb0686f52508371b253f8cfde523b1c955725346
crossfire3001 <obiwan2035@mail.ru>
2023-07-26 12:13:26 +0500

    categories in React

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,20 +3,21 @@ import React from "react";
 function Categories() {
   const [activeIndex, setActiveIndex] = React.useState(0);
 
-  const onClickCategory = () => {
-    setActiveIndex(3);
+  const categories = ['Все', 'Мясные', 'Вегетаринские', 'Гриль', 'Острые', 'Закрытые'];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
   }
 
 
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {
+          categories.map((value, i) => 
+            <li onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ""}>{value}</li>
+          )
+        }
       </ul>
     </div>
   );
